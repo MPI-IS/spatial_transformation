0639887725bb06a781c654f6dce89dbeb7b96ba5
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2023-03-07 18:39:07 +0100

    Add function to compute transform from point clouds

--- include/spatial_transformation/pointcloud.hpp
+++ include/spatial_transformation/pointcloud.hpp
@@ -4,9 +4,41 @@
  * @file
  * @brief Utility functions for computing transform between point clouds.
  * @copyright 2023 Max Planck Gesellschaft.  All rights reserved.
+ *
+ * The functions of this library can be used to compute the transformation
+ * between two reference frames, given the positions of a point cloud in both
+ * frames.
+ *
+ * Example:
+ *
+ * JSON file ``points.json``:
+ *
+ * @code{.json}
+ *   [
+ *     {"foo": [-0.79, 2.11, 0.27], "bar": [-0.54, -0.87, -0.62]},
+ *     {"foo": [-0.93, 2.36, 0.26], "bar": [-0.68, -0.62, -0.63]},
+ *     ...
+ *   ]
+ * @endcode
+ *
+ * Code to load the file and compute the transformation:
+ *
+ * @code{.cpp}
+ *   // load point clouds from file
+ *   const auto points = read_point_clouds_from_json_file(
+ *           "points.json", "foo", "bar");
+ *
+ *   // compute transformation
+ *   const auto &[transformation, mean_error] =
+ *       compute_transformation_between_point_clouds(
+ *           points.first, points.second);
+ * @endcode
  */
 #pragma once
 
+#include <filesystem>
+
+#include <spdlog/logger.h>
 #include <Eigen/Eigen>
 
 #include "thirdparty/json.hpp"
@@ -33,7 +65,8 @@ double compute_mean_transform_error(Eigen::Matrix3Xd source_points,
                                     Eigen::Isometry3d transform);
 
 /**
- * @brief Extract 3d point cloud in two frames from JSON data structure.
+ * @brief Extract 3d point cloud in two reference frames from a JSON data
+ * structure.
  *
  * The JSON data is expected to be structured as a sequence of objects where
  * each object contains two members with names specified by first_key and
@@ -43,13 +76,13 @@ double compute_mean_transform_error(Eigen::Matrix3Xd source_points,
  *
  * Example (where "foo" and "bar" are first and second key):
  *
- * .. code-block:: json
- *
+ * @code{.json}
  *    [
  *      {"foo": [-0.79, 2.11, 0.27], "bar": [-0.54, -0.87, -0.62]},
  *      {"foo": [-0.93, 2.36, 0.26], "bar": [-0.68, -0.62, -0.63]},
  *      ...
  *    ]
+ * @endcode
  *
  * The positions are extracted and stored in a pair of 3xN matrices.
  *
@@ -65,5 +98,50 @@ std::pair<Eigen::Matrix3Xd, Eigen::Matrix3Xd> json_point_cloud_to_eigen(
     const json &json_data,
     const std::string &first_key,
     const std::string &second_key);
+
+/**
+ * @brief Read 3d point cloud in two reference frames from a JSON file.
+ *
+ * Regarding the expected structure of the JSON file and the meaning of first
+ * and second key, see @ref json_point_cloud_to_eigen.
+ *
+ * @param json_file  Path to the JSON file.
+ * @param first_key Name of the entries containing points in the first frame.
+ * @param second_key Name of the entries containing points in the second frame.
+ *
+ * @return Pair of 3xN matrices.  The first element contains all the points
+ *      corresponding the first_key, the second element all the points
+ *      corresponding to second_key.
+ *
+ * @throw std::system_error  If opening the file fails.
+ * @throw std::runtime_error  If the file is not valid JSON or does not have the
+ *      expected structure.
+ */
+std::pair<Eigen::Matrix3Xd, Eigen::Matrix3Xd> read_point_clouds_from_json_file(
+    const std::filesystem::path &json_file,
+    const std::string &first_key,
+    const std::string &second_key);
+
+/**
+ * @brief Compute transformation between two point clouds.
+ *
+ * Expects as input two point clouds and computes a transformation that maps the
+ * first one to the second.  This can be used to find the transformation between
+ * two reference frames if a set of points is given in both frames.
+ *
+ * The two point clouds are expected to be of equal size and need to be ordered,
+ * i.e. point ``from_points[i]`` corresponds to ``to_points[i]`` for all ``i``.
+ *
+ * @param from_points  Points in reference frame A.
+ * @param to_points  Same points in reference frame B.
+ *
+ * @return Tuple ``(tf, mean_error)`` with
+ *      - ``tf``: the transformation from A to B and
+ *      - ``mean_error``: the mean absolute error of the transformation based on
+ *         the given point clouds.
+ */
+std::tuple<Eigen::Isometry3d, double>
+compute_transformation_between_point_clouds(const Eigen::Matrix3Xd &from_points,
+                                            const Eigen::Matrix3Xd &to_points);
 
 }  // namespace spatial_transformation
--- src/pointcloud.cpp
+++ src/pointcloud.cpp
@@ -2,6 +2,11 @@
 
 #include <spatial_transformation/pointcloud.hpp>
 
+#include <fstream>
+
+#include <fmt/core.h>
+#include <fmt/ostream.h>
+
 namespace spatial_transformation
 {
 double compute_mean_transform_error(Eigen::Matrix3Xd source_points,
@@ -46,6 +51,58 @@ std::pair<Eigen::Matrix3Xd, Eigen::Matrix3Xd> json_point_cloud_to_eigen(
     }
 
     return std::make_pair(first_points, second_points);
+}
+
+std::pair<Eigen::Matrix3Xd, Eigen::Matrix3Xd> read_point_clouds_from_json_file(
+    const std::filesystem::path &json_file,
+    const std::string &first_key,
+    const std::string &second_key)
+{
+    std::ifstream in_stream(json_file);
+    if (in_stream.fail())
+    {
+        throw std::system_error(
+            errno,
+            std::system_category(),
+            fmt::format("Failed to open file '{}'", json_file.string()));
+    }
+
+    json trajectory;
+    try
+    {
+        in_stream >> trajectory;
+    }
+    catch (const std::exception &e)
+    {
+        throw std::runtime_error(
+            fmt::format("Failed to parse JSON file '{}': {}",
+                        json_file.string(),
+                        e.what()));
+    }
+
+    try
+    {
+        return json_point_cloud_to_eigen(trajectory, first_key, second_key);
+    }
+    catch (const std::exception &e)
+    {
+        throw std::runtime_error(
+            fmt::format("Failed to read point clouds from JSON: {}", e.what()));
+    }
+}
+
+std::tuple<Eigen::Isometry3d, double>
+compute_transformation_between_point_clouds(const Eigen::Matrix3Xd &from_points,
+                                            const Eigen::Matrix3Xd &to_points)
+{
+    // compute transformation using Umeyama algorithm
+    Eigen::Isometry3d tf;
+    tf.matrix() = Eigen::umeyama(from_points, to_points, false);
+
+    double mean_error =
+        compute_mean_transform_error(from_points, to_points, tf);
+
+    return {tf, mean_error};
 }
 
 }  // namespace spatial_transformation
